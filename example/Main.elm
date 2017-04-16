25e5b8de1ba539a2d139a0d141d045f0b8ff019b
Marek Romanowski <marek.romanowski@gmail.com>
2017-04-17 01:21:06 +0200

    Add new test files. Slightly improve view. Fix manual update.

--- example/Main.elm
+++ example/Main.elm
@@ -15,33 +15,59 @@ import Combine exposing (ParseResult)
 
 
 type Msg
-    = Replace String
-    | Loaded (Result Error String)
+    = Replace String String
+    | Loaded String (Result Error String)
 
 
-type alias Item = (String, Result (Combine.ParseErr ()) (Combine.ParseOk () (List Statement)))
+type alias Item = (String, String, Result (Combine.ParseErr ()) (Combine.ParseOk () (List Statement)))
 type alias Model = List Item
 
 
-files =
-    [ "Array"
-    , "Basics"
-    , "Bitwise"
-    , "Char"
-    , "Color"
-    , "Date"
-    , "Debug"
+testFiles =
+    [ ("elm-lang/core/5.1.1/src/",
+        [ "Array"
+        , "Basics"
+        , "Bitwise"
+        , "Char"
+        , "Color"
+        , "Date"
+        , "Debug"
+        , "Dict"
+        , "Json/Decode"
+        , "Json/Encode"
+        , "List"
+        , "Maybe"
+        , "Platform"
+        , "Platform/Cmd"
+        , "Platform/Sub"
+        , "Process"
+        , "Random"
+        , "Regex"
+        , "Result"
+        , "Set"
+        , "String"
+        , "Task"
+        , "Time"
+        , "Tuple"
+        ])
+    , ("elm-lang/http/1.0.0/src/",
+        [ "Http"
+        , "Http/Progress"
+        ])
     ]
 
 
 init : (Model, Cmd Msg)
 init =
-    [ ( sampleModule, Ast.parse sampleModule )
+    [ ( "custom", sampleModule, Ast.parse sampleModule )
     ]
     !
-    (List.map (\file ->
-        send Loaded (getString <| "https://raw.githubusercontent.com/elm-lang/core/5.1.1/src/" ++ file ++ ".elm")
-    ) files
+    ((List.concatMap (\(package, files) ->
+        List.map (\file ->
+            send (Loaded (package ++ " - " ++ file))
+                (getString <| "https://raw.githubusercontent.com/" ++ package ++ file ++ ".elm")
+        ) files
+    )) testFiles
     )
 
 
@@ -76,12 +102,16 @@ withChild title children =
 update : Msg -> Model -> (Model, Cmd Msg)
 update action model =
     case action of
-        Replace m ->
-            model ! []
-        Loaded result ->
+        Replace name m ->
+            List.map (\(elementName, str, ast) ->
+                case elementName == name of
+                    True -> (elementName, m, Ast.parse m)
+                    False -> (elementName, str, ast)
+            ) model ! []
+        Loaded name result ->
             case result of
                 Ok data ->
-                    (( data, Ast.parse data ) :: model)
+                    (( name, data, Ast.parse data ) :: model)
                     ! []
                 Err err ->
                     let
@@ -93,7 +123,7 @@ update action model =
 withChild : a -> List (Html Msg) -> Html Msg
 withChild title children =
     li []
-        [ pre [] [ text <| toString title ]
+        [ text <| toString title
         , ul [] children
         ]
 
@@ -111,7 +141,7 @@ expression e =
                 ]
 
         e ->
-            li [] [ pre [] [ text <| toString e ] ]
+            li [] [ text <| toString e ]
 
 
 statement : Statement -> Html Msg
@@ -121,43 +151,81 @@ statement s =
             withChild s [ expression e ]
 
         s ->
-            li [] [ pre [] [ text <| toString s ] ]
+            li [] [ text <| toString s ]
 
 
 tree : Item -> Html Msg
-tree (moduleText, ast) =
+tree (name, moduleText, ast) =
     case ast of
         ( Ok (_, _, statements)) ->
             ul [] (List.map statement statements)
 
         err ->
-            div [] [ text <| toString err ]
+            text <| toString err
+
+
+countItems value model =
+    List.filter (\i ->
+        case i of
+            (_, _, Ok _) -> value
+            _ -> not value
+    ) model
+    |> List.length
 
 
 view : Model -> Html Msg
 view model =
     div []
-        (List.map (\(txt, ast) ->
-            div []
-                [ textarea
-                    [ on "input" (JD.map Replace targetValue)
-                    , style
-                        [ ( "height", "600px" )
-                        , ( "width", "30%" )
-                        , ( "display", "inline-block" )
+        [ h1 []
+            [ text "All items: "
+            , text <| toString <| List.length model
+            , text " Success count: "
+            , text <| toString <| countItems True model
+            , text " Failure count: "
+            , text <| toString <| countItems False model
+            ]
+        , table
+            [ style [ ( "width",  "100%" ) ]
+            ]
+            [ tbody []
+                (List.map (\(name, txt, ast) ->
+                    tr
+                        [ style [ ( "width",  "100%" ) ]
                         ]
-                    ]
-                    [ text txt ]
-                , div
-                    [ style
-                        [ ( "display", "inline-block" )
-                        , ( "width", "69%" )
-                        , ( "vertical-align", "top" )
+                        [ td
+                            [ style
+                                [ ( "width",  "30%" )
+                                , ( "position", "relative" )
+                                ]
+                            ]
+                            [ textarea
+                                [ on "input" (JD.map (Replace name) targetValue)
+                                , style
+                                    [ ( "width", "100%" )
+                                    , ( "padding", "0" )
+                                    , ( "position", "absolute" )
+                                    , ( "top", "0" )
+                                    , ( "bottom", "0" )
+                                    , ( "left", "0" )
+                                    , ( "right", "0" )
+                                    ]
+                                ]
+                                [ text txt ]
+                            ]
+                        , td
+                            [ style [ ( "width",  "70%" )
+                                    , ( "vertical-align", "top" )
+                                    , ( "background-color" ,
+                                        case ast of
+                                            Ok _ -> "green"
+                                            _ -> "red" )
+                                    ]
+                            ]
+                            [ tree (name, txt, ast) ]
                         ]
-                    ]
-                    [ tree (txt, ast) ]
-                ]
-        ) model)
+                ) model)
+            ]
+        ]
 
 
 main : Program Never Model Msg
