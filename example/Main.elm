2326704227dc98b886f25ed0965e900cb1970417
Marek Romanowski <marek.romanowski@gmail.com>
2017-04-17 00:07:51 +0200

    Add requests for a few files from core package. Parse and display them

--- example/Main.elm
+++ example/Main.elm
@@ -7,15 +7,45 @@ import Html exposing (..)
 import Html
 import Html.Attributes exposing (..)
 import Html.Events exposing (..)
+import Http exposing (..)
 import Json.Decode as JD
+import Platform.Sub
+
+import Combine exposing (ParseResult)
 
 
 type Msg
     = Replace String
+    | Loaded (Result Error String)
 
 
-init : String
+type alias Item = (String, Result (Combine.ParseErr ()) (Combine.ParseOk () (List Statement)))
+type alias Model = List Item
+
+
+files =
+    [ "Array"
+    , "Basics"
+    , "Bitwise"
+    , "Char"
+    , "Color"
+    , "Date"
+    , "Debug"
+    ]
+
+
+init : (Model, Cmd Msg)
 init =
+    [ ( sampleModule, Ast.parse sampleModule )
+    ]
+    !
+    (List.map (\file ->
+        send Loaded (getString <| "https://raw.githubusercontent.com/elm-lang/core/5.1.1/src/" ++ file ++ ".elm")
+    ) files
+    )
+
+
+sampleModule =
     """module Main exposing (..)
 
 f : Int -> Int
@@ -43,11 +73,21 @@ withChild title children =
 """
 
 
-update : Msg -> String -> String
+update : Msg -> Model -> (Model, Cmd Msg)
 update action model =
     case action of
         Replace m ->
-            m
+            model ! []
+        Loaded result ->
+            case result of
+                Ok data ->
+                    (( data, Ast.parse data ) :: model)
+                    ! []
+                Err err ->
+                    let
+                        x = Debug.log "error" err
+                    in
+                        model ! []
 
 
 withChild : a -> List (Html Msg) -> Html Msg
@@ -84,9 +124,9 @@ statement s =
             li [] [ pre [] [ text <| toString s ] ]
 
 
-tree : String -> Html Msg
-tree m =
-    case Ast.parse m of
+tree : Item -> Html Msg
+tree (moduleText, ast) =
+    case ast of
         ( Ok (_, _, statements)) ->
             ul [] (List.map statement statements)
 
@@ -94,33 +134,39 @@ tree m =
             div [] [ text <| toString err ]
 
 
-view : String -> Html Msg
+view : Model -> Html Msg
 view model =
     div []
-        [ textarea
-            [ on "input" (JD.map Replace targetValue)
-            , style
-                [ ( "height", "600px" )
-                , ( "width", "30%" )
-                , ( "display", "inline-block" )
+        (List.map (\(txt, ast) ->
+            div []
+                [ textarea
+                    [ on "input" (JD.map Replace targetValue)
+                    , style
+                        [ ( "height", "600px" )
+                        , ( "width", "30%" )
+                        , ( "display", "inline-block" )
+                        ]
+                    ]
+                    [ text txt ]
+                , div
+                    [ style
+                        [ ( "display", "inline-block" )
+                        , ( "width", "69%" )
+                        , ( "vertical-align", "top" )
+                        ]
+                    ]
+                    [ tree (txt, ast) ]
                 ]
-            ]
-            [ text model ]
-        , div
-            [ style
-                [ ( "display", "inline-block" )
-                , ( "width", "69%" )
-                , ( "vertical-align", "top" )
-                ]
-            ]
-            [ tree model ]
-        ]
+        ) model)
 
 
-main : Program Never String Msg
+main : Program Never Model Msg
 main =
-    Html.beginnerProgram
-        { model = init
+    Html.program
+        { init = init
         , update = update
+        , subscriptions = \m -> Sub.none
         , view = view
         }
+
+
